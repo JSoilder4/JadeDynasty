f082ea69349bb16ec0ccc7404bff20bfa9883a0a
JSoilder4 <winkler21960@gmail.com>
2022-09-27 22:52:09 -0400

    invincibility frames

--- Assets/Scripts/playerMove.cs
+++ Assets/Scripts/playerMove.cs
@@ -27,6 +27,11 @@ public class playerMove : MonoBehaviour
 
     public hp hp;
 
+    public bool invuln;
+    public SpriteRenderer sprite;
+    public float invulnTime = 0.5f;
+    public bool invulnFrameRunning;
+
     private void Awake()
     {
         hp = GetComponent<hp>();
@@ -35,7 +40,7 @@ public class playerMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        sprite = GetComponent<SpriteRenderer>();
         coords = new int[2];
         pms = this;
         rb = GetComponent<Rigidbody2D>();
@@ -69,7 +74,8 @@ public class playerMove : MonoBehaviour
                
             }
         }
-        
+
+
 
         
         
@@ -99,31 +105,48 @@ public class playerMove : MonoBehaviour
         {
             rb.velocity = velocity * speed * Time.fixedDeltaTime;
         }
-        else if(dodging)
+        else if (dodging)
         {
-            dodgeTime -= Time.fixedDeltaTime;
+            dodgeChecking();
+        }
+        if (!invuln)
+        {
+            StopCoroutine(flash());
+        }
 
-            if (dodgeTime > 0.2)
+
+
+        //rb.MovePosition(transform.position + velocity * Time.fixedDeltaTime);
+    }
+    public void dodgeChecking()
+    {
+        dodgeTime -= Time.fixedDeltaTime;
+
+        if (dodgeTime > 0.2)
+        {
+            rb.velocity = velocity * dodgeSpeed * Time.fixedDeltaTime;
+            invuln = true;
+        }
+        else if (dodgeTime <= 0.2)
+        {
+            rb.velocity = velocity * (dodgeSpeed / 2) * Time.fixedDeltaTime;
+            if (!invulnFrameRunning)
             {
-                rb.velocity = velocity * dodgeSpeed * Time.fixedDeltaTime;
-            }
-            else if (dodgeTime <= 0.2)
-            {
-                rb.velocity = velocity * (dodgeSpeed / 2)*Time.fixedDeltaTime;
+                invuln = false;
             }
             
+        }
+
             //dodgeSpeed -= dodgeSpeed * dodgeSpeedDropMulti * Time.fixedDeltaTime;
             //rb.velocity = velocity * dodgeSpeed;
-            if (dodgeTime <= 0)
-            {
-                dodging = false;
-                dodgeSpeed = dodgeSpeedOG;
-                //dodgeCDTimer = dodgeCDTimerOG;
-                dodgeTime = dodgeTimeOG;
-            }
-        }
-           
-            //rb.MovePosition(transform.position + velocity * Time.fixedDeltaTime);
+        if (dodgeTime <= 0)
+         {
+            dodging = false;
+            dodgeSpeed = dodgeSpeedOG;
+            //dodgeCDTimer = dodgeCDTimerOG;
+            dodgeTime = dodgeTimeOG;
+         }
+        
     }
     private void LateUpdate()
     {
@@ -161,8 +184,13 @@ public class playerMove : MonoBehaviour
         {
             //collision.GetComponent<enemy>().hp = 1000;
             //die();
-            hp.takeDamage(1);
-            if (collision.CompareTag("arrow"))
+            if (!invuln)
+            {
+                hp.takeDamage(1);
+                StartCoroutine(invulnFrame());
+            }
+            
+            if (collision.CompareTag("arrow") && !invuln)
             {
                 Destroy(collision.gameObject);
             }
@@ -179,15 +207,46 @@ public class playerMove : MonoBehaviour
             
         }
     }
+    
+    public IEnumerator invulnFrame()
+    {
+        // StopAllCoroutines();
+        invulnFrameRunning = true;
+       // print("haha");
+        StartCoroutine(flash());
+        invuln = true;
+        yield return new WaitForSeconds(invulnTime);
+       // print("hoho");
+        StopCoroutine(flash());
+        invuln = false;
+        invulnFrameRunning = false;
+        yield return null;
+    }
+    public IEnumerator flash()
+    {
+
+        for (int i = 0; i < 6; i++)
+        {
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 0);
+            yield return new WaitForSeconds(invulnTime/12);
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 255);
+            yield return new WaitForSeconds(invulnTime/12);
+        }
+
+
+
+        yield return null;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.CompareTag("Enemy"))
+        if (collision.transform.CompareTag("Enemy") && !invuln)
         {
             //collision.transform.GetComponent<enemy>().hp = 1000;
             //die();
             hp.takeDamage(1);
-            print("yeah?:");
-            collision.gameObject.GetComponent<Knockback>().knockback(collision.transform.position - transform.position, 1000);
+            StartCoroutine(invulnFrame());
+            //print("yeah?:");
+            //collision.gameObject.GetComponent<Knockback>().knockback(collision.transform.position - transform.position, 1000);
             //collision.gameObject.GetComponent<Rigidbody2D>().AddForce((collision.transform.position - transform.position) * 5000, ForceMode2D.Impulse);
 
         }
@@ -195,6 +254,15 @@ public class playerMove : MonoBehaviour
         {
             collision.transform.GetComponent<enemy>().hp.currentHP = 1000;
             
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Enemy") && !invuln)
+        {
+            hp.takeDamage(1);
+            StartCoroutine(invulnFrame());
+
         }
     }
     public void die()
