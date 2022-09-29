6d30ed951e3fcb46531be5030d2dc89476d35633
JSoilder4 <73358291+JSoilder4@users.noreply.github.com>
2022-09-28 21:29:41 -0400

    should work and be cool:tm:

--- Assets/ProfCatnip.cs
+++ Assets/ProfCatnip.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class ProfCatnip : enemy
 {
     public Vector3 velocity;
+    public LayerMask theLayer;// = LayerMask.GetMask("Player");
+
+
+    public float timer;
+    public float timerOG;
+    public GameObject arrow;
+    public GameObject shootPoint;
+
+    public bool shooting;
+
 
     // Start is called before the first frame update
     public override void Start()
     {
         base.Start();
+        theLayer = LayerMask.GetMask("Player");
+        shootPoint = GetComponentInChildren<GameObject>();
         // sprite = GetComponent<SpriteRenderer>();
     }
 
@@ -28,6 +40,7 @@ public class ProfCatnip : enemy
     }
     public void attackPlayer()
     {
+        timer -= Time.deltaTime;
         velocity = player.transform.position - transform.position;
         if (velocity.magnitude > 5.2f)
         {
@@ -40,11 +53,32 @@ public class ProfCatnip : enemy
             //print("gwoodbwye");
         }
 
-
+        RaycastHit2D raycast = Physics2D.Raycast(shootPoint.transform.position, player.transform.position - shootPoint.transform.position, 10f, theLayer);
+        Debug.DrawRay(shootPoint.transform.position, player.transform.position - shootPoint.transform.position, Color.red);
+        if (raycast && raycast.transform.CompareTag("Player"))
+        {
+            print("FIRE!");
+            shoot();
+        }
 
 
     }
-        
+    public void shoot()
+    {
+        Vector3 lookDir = player.transform.position - shootPoint.transform.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        if (timer <= timerOG / 4)
+        {
+            shooting = true;
+        }
+        if (timer <= 0)
+        {
+            GameObject g = Instantiate(arrow, shootPoint.transform.position, Quaternion.Euler(0, 0, angle));
+            // GameManager.GM.addSpawnedObject(g);
+            shooting = false;
+            timer = timerOG;
+        }
+    }
 
 
 
