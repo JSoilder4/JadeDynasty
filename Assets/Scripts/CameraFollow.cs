7ed233388cbe7c08e83591a8205c4d64048bf6a5
JSoilder4 <winkler21960@gmail.com>
2022-10-04 19:55:40 -0400

    sadge

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,39 +5,60 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
 
+    public enum CamType
+    {
+        gridBased,
+        followAndMouse
+    }
+    public CamType cam;
+
     public GameObject player;
     // public Camera cam;
 
     public float leftrightNum;
 
+    public Camera camVar;
+
+    public Vector3 mousePos;
+
     // Start is called before the first frame update
     void Start()
     {
-        //cam = GetComponent<Camera>();
+        camVar = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.transform.position.x >= transform.position.x + leftrightNum)
+        mousePos = camVar.ScreenToWorldPoint(Input.mousePosition);
+        switch (cam)
         {
-            transform.position = new Vector3(transform.position.x+ (leftrightNum*2), transform.position.y,transform.position.z);
-            playerMove.pms.coords[0] += 1;
+            case CamType.gridBased:
+                if (player.transform.position.x >= transform.position.x + leftrightNum)
+                {
+                    transform.position = new Vector3(transform.position.x + (leftrightNum * 2), transform.position.y, transform.position.z);
+                    playerMove.pms.coords[0] += 1;
+                }
+                else if (player.transform.position.x <= transform.position.x - leftrightNum)
+                {
+                    transform.position = new Vector3(transform.position.x - (leftrightNum * 2), transform.position.y, transform.position.z);
+                    playerMove.pms.coords[0] -= 1;
+                }
+                else if (player.transform.position.y >= transform.position.y + 5f)
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y + 10f, transform.position.z);
+                    playerMove.pms.coords[1] += 1;
+                }
+                else if (player.transform.position.y <= transform.position.y - 5f)
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y - 10f, transform.position.z);
+                    playerMove.pms.coords[1] -= 1;
+                }
+                break;
+            case CamType.followAndMouse:
+                transform.position = player.transform.position - mousePos;
+                break;
         }
-        else if(player.transform.position.x <= transform.position.x - leftrightNum)
-        {
-            transform.position = new Vector3(transform.position.x - (leftrightNum*2), transform.position.y, transform.position.z);
-            playerMove.pms.coords[0] -= 1;
-        }
-        else if (player.transform.position.y >= transform.position.y+5f)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y+10f, transform.position.z);
-            playerMove.pms.coords[1] += 1;
-        }
-        else if (player.transform.position.y <= transform.position.y - 5f)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y-10f, transform.position.z);
-            playerMove.pms.coords[1] -= 1;
-        }
+        
     }
 }
